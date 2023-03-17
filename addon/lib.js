2fb65c96724395d9d3b795b09a3e49133d3ffd1d
Cheng <cz@sz>
2023-03-17 13:25:49 +1100

    moment-fix

--- addon/lib.js
+++ addon/lib.js
@@ -1,3 +1,15 @@
 /* globals self */
+import config from 'ember-get-config';
 
-export default self.moment;
+let moment;
+const localeOutputPath = config.moment && config.moment.includeTimezone;
+
+if (typeof self.FastBoot === 'undefined') {
+  moment = self.moment;
+} else if (localeOutputPath) {
+  moment = self.FastBoot.require('moment-timezone');
+} else {
+  moment = self.FastBoot.require('moment');
+}
+
+export default moment;
